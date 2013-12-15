267a52ae626de1156570b21c6ae478265a4a5075
xpetrom24 <xavierpetromelis@xavier-petromeliss-MacBook-Pro.local>
2013-12-15 17:51:51 -0500

    adding background image changes to website

---
+++ script2.js
@@ -0,0 +1,23 @@
+var buttons = ["box1","box2","box3","box4"];
+
+        $.each(buttons, function(index, element){
+                $("<div class='manipulator' id='"+element+"' />").appendTo("body");
+        });
+
+
+        $(".manipulator").each(function(index, element){
+                var currentID = $(element).attr("id");
+                var currentIDselector = "#" + currentID;
+                $(currentIDselector).click(function(){
+                        
+                        console.log(currentID);
+                        $.each(buttons, function(index, element){
+                                $("body").removeClass(element);
+                        });
+                        $("body").addClass(currentID);
+
+                });
+
+        });
+
+        console.log("done");
