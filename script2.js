c52d21ff668b82eb176b72b3e41fe2f6fea5ada4
xpetrom24 <xavierpetromelis@xavier-petromeliss-MacBook-Pro.local>
2013-12-15 18:33:02 -0500

    attempt at altering background not succeeding

--- script2.js
+++ script2.js
@@ -1,23 +1,25 @@
-var buttons = ["box1","box2","box3","box4"];
+$(document).ready(function(){
+	var buttons = [,"small","medium","large","xlarge"];
 
-        $.each(buttons, function(index, element){
-                $("<div class='manipulator' id='"+element+"' />").appendTo("body");
-        });
+			$.each(buttons, function(index, element){
+					$("<div class='manipulator' id='"+element+"' />").appendTo("body");
+			});
 
 
-        $(".manipulator").each(function(index, element){
-                var currentID = $(element).attr("id");
-                var currentIDselector = "#" + currentID;
-                $(currentIDselector).click(function(){
-                        
-                        console.log(currentID);
-                        $.each(buttons, function(index, element){
-                                $("body").removeClass(element);
-                        });
-                        $("body").addClass(currentID);
+			$(".manipulator").each(function(index, element){
+					var currentID = $(element).attr("id");
+					var currentIDselector = "#" + currentID;
+					$(currentIDselector).click(function(){
+						
+							console.log(currentID);
+							$.each(buttons, function(index, element){
+									$("body").removeClass(element);
+							});
+							$("body").addClass(currentID);
 
-                });
+					});
 
-        });
+			});
 
-        console.log("done");
+			console.log("done");
+}
