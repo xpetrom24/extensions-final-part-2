16b83d3af68eaa0e63d3d2b03e8abca2a01dee29
xpetrom24 <xavierpetromelis@xavier-petromeliss-MacBook-Pro.local>
2013-12-15 20:56:59 -0500

    made changes to syntax but buttons only work for chrome sample extension?

--- script2.js
+++ script2.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 	var buttons = ["small","medium","large","xlarge"];
 
 			$.each(buttons, function(index, element){
-					$("<div class='manipulator' id='"+element+"' />").appendTo("body");
+					$("<div class='boxes' id='"+element+"' />").appendTo("body");
 			});
 
 
-			$(".manipulator").each(function(index, element){
+			$(".boxes").each(function(index, element){
 					var currentID = $(element).attr("id");
 					var currentIDselector = "#" + currentID;
 					$(currentIDselector).click(function(){
